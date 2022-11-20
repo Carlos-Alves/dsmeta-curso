63dde9a483460a15b82e17a031e1a3b7603cd0a6
Carlos-H-Alves <carlos.alves.ti@gmail.com>
2022-11-19 22:18:40 -0300

    method get

---
+++ backend/src/main/java/com/devsuperior/curso/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.curso.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.curso.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
